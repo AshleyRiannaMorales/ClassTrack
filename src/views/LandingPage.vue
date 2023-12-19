a4e99ceb70c1ee50adb2e9f49aac50346ce28c09
Ash <amorales_220000000684@uic.edu.ph>
2023-12-19 14:37:38 +0800

    Update LandingPage.vue

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -3,6 +3,11 @@ import Button from 'primevue/button';
 </script>
 
 <template>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap" rel="stylesheet">
+    
+
   <div class="wrapper">
 
     <!-- Side Bar -->
