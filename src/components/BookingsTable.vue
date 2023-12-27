6769dd26f58ae36e0b9abffd2656a845f98451d3
Ash <amorales_220000000684@uic.edu.ph>
2023-12-28 07:54:02 +0800

    update BookingsTable.vue

--- src/components/BookingsTable.vue
+++ src/components/BookingsTable.vue
@@ -2,7 +2,6 @@
 import Button from 'primevue/button';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
-import ConfirmDialog from 'primevue/confirmdialog';
 
 import { ref } from "vue";
 
@@ -17,32 +16,18 @@ const booking = ref([
         bookingStartTime: "14:30",
         bookingEndTime: "16:00",
         bookingStatus: "Approved"
+    },
+    {
+        id: 2,
+        instructorID: 500002,
+        roomNum: 202,
+        bookingRequestDate: "25 December 2023",
+        bookingPurpose: "Class",
+        bookingStartTime: "10:00",
+        bookingEndTime: "12:00",
+        bookingStatus: "Pending"
     }
 ]);
-
-const displayConfirmDialog = ref(false);
-const selectedBooking = ref(null);
-
-const showConfirmDialog = (rowData) => {
-    selectedBooking.value = rowData;
-    displayConfirmDialog.value = true;
-};
-
-const confirmAction = (approved) => {
-    // Logic to execute when the user clicks "Yes" or "No" in the ConfirmDialog
-    displayConfirmDialog.value = false;
-
-    if (approved) {
-        // Logic to update the booking status to "Approved"
-        selectedBooking.value.bookingStatus = "Approved";
-    }
-};
-
-const rejectAction = () => {
-    // Logic to execute when the user clicks "No" in the ConfirmDialog
-    displayConfirmDialog.value = false;
-};
-
 
 
 </script>
@@ -56,20 +41,11 @@ const rejectAction = () => {
             <Column field="bookingStartTime" header="Start Time" style="color: #DD385A;"></Column>
             <Column field="bookingEndTime" header="End Time" style="color: #DD385A;"></Column>
             <Column field="bookingStatus" header="Status" style="color: #DD385A;">
-                <template #body="{ rowData }">
-                    <Button :label="rowData.bookingStatus === 'Pending' ? 'Pending' : 'Approved'"
-                        :icon="rowData.bookingStatus === 'Pending' ? 'pi pi-spinner' : 'pi pi-check'"
-                        :class="rowData.bookingStatus === 'Pending' ? 'p-button-info' : 'p-button-success'"
-                        @click="rowData.bookingStatus === 'Pending' ? showConfirmDialog(rowData) : null" />
+                <template #body="rowData">
+                    <Button label="Pending" icon="pi pi-spinner" class="p-button-info" id="pendingStatus" />
                 </template>
             </Column>
         </DataTable>
-
-        <ConfirmDialog v-model="displayConfirmDialog" @confirm="() => confirmAction(true)" @reject="rejectAction">
-            <template #message>
-                <span>Do you want to approve this booking request?</span>
-            </template>
-        </ConfirmDialog>
     </div>
 </template>
 
@@ -84,6 +60,10 @@ button {
 }
 
 .p-button-info {
+    
+}
+
+#pendingStatus {
     background-color: #fff0f0;
     color: #DD385A;
 }
