9c9ce69ac1f4501c47d7d21f9fcc8f4d90ff222c
Ash <amorales_220000000684@uic.edu.ph>
2023-12-28 07:40:57 +0800

    update

--- src/components/BookingsTable.vue
+++ src/components/BookingsTable.vue
@@ -81,13 +81,12 @@ const rejectAction = () => {
 
 button {
     padding: 5px;
-    font-family: 'Inter', sans-serif;
 }
 
 .p-button-info {
     background-color: #fff0f0;
     color: #DD385A;
-    
+    font-family: 'Inter', sans-serif;
 }
 </style>
 
