760bf9406bb58690c4dbaa35bb6adcf177492362
Ash <amorales_220000000684@uic.edu.ph>
2024-04-14 18:56:49 +0800

    TRIAL update

--- src/views/AdminBookings.vue
+++ src/views/AdminBookings.vue
@@ -3,6 +3,44 @@ import SideBarMenu from "../components/SideBarMenu.vue";
 import TopBarMenu from "../components/TopBarMenu.vue";
 import AdminBookingsTable from "../components/AdminBookingsTable.vue";
 
+import MultiSelect from 'primevue/multiselect';
+
+import { ref } from "vue";
+
+const selectedCities = ref();
+const groupedCities = ref([
+    {
+        label: 'Germany',
+        code: 'DE',
+        items: [
+            { label: 'Berlin', value: 'Berlin' },
+            { label: 'Frankfurt', value: 'Frankfurt' },
+            { label: 'Hamburg', value: 'Hamburg' },
+            { label: 'Munich', value: 'Munich' }
+        ]
+    },
+    {
+        label: 'USA',
+        code: 'US',
+        items: [
+            { label: 'Chicago', value: 'Chicago' },
+            { label: 'Los Angeles', value: 'Los Angeles' },
+            { label: 'New York', value: 'New York' },
+            { label: 'San Francisco', value: 'San Francisco' }
+        ]
+    },
+    {
+        label: 'Japan',
+        code: 'JP',
+        items: [
+            { label: 'Kyoto', value: 'Kyoto' },
+            { label: 'Osaka', value: 'Osaka' },
+            { label: 'Tokyo', value: 'Tokyo' },
+            { label: 'Yokohama', value: 'Yokohama' }
+        ]
+    }
+]);
+
 </script>
 
 <template>
@@ -11,20 +49,33 @@ import AdminBookingsTable from "../components/AdminBookingsTable.vue";
 
     <div class="adminBookings-container">
         <span class="greetings">Bookings</span>
+
+        <div class="instructorBookings-buttons">
+
+            <div class="card flex justify-content-center">
+                <MultiSelect v-model="selectedCities" :options="groupedCities" optionLabel="label"
+                    optionGroupLabel="label" optionGroupChildren="items" display="chip" placeholder="Select Cities"
+                    class="w-full md:w-20rem">
+
+                </MultiSelect>
+            </div>
+
+        </div>
+
         <AdminBookingsTable />
     </div>
 
-    
+
 </template>
 
 <style scoped>
-
 .adminBookings-container {
     margin-left: 60px;
     padding: 70px 100px;
     font-family: 'Inter', sans-serif;
     color: #DD385A;
 }
+
 .greetings {
     font-size: 40px;
     font-weight: 700;
--- src/views/InstructorBookings.vue
+++ src/views/InstructorBookings.vue
@@ -2,43 +2,7 @@
 import SideBarMenu from "../components/SideBarMenu.vue";
 import TopBarMenu from "../components/TopBarMenu.vue";
 import InstructorBookingsTable from "../components/InstructorBookingsTable.vue";
-import MultiSelect from 'primevue/multiselect';
 
-import { ref } from "vue";
-
-const selectedCities = ref();
-const groupedCities = ref([
-    {
-        label: 'Germany',
-        code: 'DE',
-        items: [
-            { label: 'Berlin', value: 'Berlin' },
-            { label: 'Frankfurt', value: 'Frankfurt' },
-            { label: 'Hamburg', value: 'Hamburg' },
-            { label: 'Munich', value: 'Munich' }
-        ]
-    },
-    {
-        label: 'USA',
-        code: 'US',
-        items: [
-            { label: 'Chicago', value: 'Chicago' },
-            { label: 'Los Angeles', value: 'Los Angeles' },
-            { label: 'New York', value: 'New York' },
-            { label: 'San Francisco', value: 'San Francisco' }
-        ]
-    },
-    {
-        label: 'Japan',
-        code: 'JP',
-        items: [
-            { label: 'Kyoto', value: 'Kyoto' },
-            { label: 'Osaka', value: 'Osaka' },
-            { label: 'Tokyo', value: 'Tokyo' },
-            { label: 'Yokohama', value: 'Yokohama' }
-        ]
-    }
-]);
 
 </script>
 
@@ -49,16 +13,7 @@ const groupedCities = ref([
     <div class="instructorBookings-container">
         <span class="greetings">Bookings</span>
 
-        <div class="instructorBookings-buttons">
-
-            <div class="card flex justify-content-center">
-                <MultiSelect v-model="selectedCities" :options="groupedCities" optionLabel="label"
-                    optionGroupLabel="label" optionGroupChildren="items" display="chip" placeholder="Select Cities" class="w-full md:w-20rem">
-                    
-                </MultiSelect>
-            </div>
-
-        </div>
+        
 
         <InstructorBookingsTable />
 
